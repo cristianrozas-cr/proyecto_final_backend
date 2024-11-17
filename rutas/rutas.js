b979b86a2184aa872613b423b068294658806e98
Cristian Rozas <cristianrozas.cr@gmail.com>
2024-11-17 16:40:09 -0300

    Se actualizan las rutas

--- rutas/rutas.js
+++ rutas/rutas.js
@@ -9,24 +9,41 @@ import { agregarFavorito, obtenerFavoritos, eliminarFavorito } from "../controll
 
 const router = Router()
 
+//Publicaciones
 router.get("/galeria", publicacionController.readGaleria); // Desplegar Galeria
 router.post("/publicaciones", publicacionController.agregarPublicacion); // Agregar Publicacion
+router.get("/publicaciones/:id", publicacionController.detallePublicacion); //Obtener detalle de una publicacion
+router.get("/usuarios/perfil/:id", publicacionController.publicacionesUsuarios); //Obtener todas las publicaciones de un usuario
+router.delete("/publicaciones/:id", publicacionController.eliminarPublicacion); //Eliminar una publicacion
+
+
+//Carrito
 router.post('/carrito', carritoController.agregarProducto); // Agregar producto a carrito
 router.get('/carrito/:usuario_id', carritoController.obtenerProductos); //Obtener carrito de ususario
 router.put('/carrito', carritoController.actualizarCantidad); // Modificar cantidad de producto en Carrito
 router.delete('/carrito', carritoController.eliminarProducto); // Eliminar producto de Carrito
+
+//Gestión de usuarios
 router.post("/login", usuarioController.loginUsuario); // Inicio de sesión + TOKEN
 router.post("/registro", usuarioController.crearUsuario) // Registro de usuario + TOKEN
 router.get("/usuario", usuarioController.tokenUsuario) // Verificacion de TOKEN
 router.put("/update_perfil", usuarioController.updateUsuario) // Actualizacion info de Usuario
 router.delete("/usuario/:id", usuarioController.borrarUsuario) // Borrar Usuario
+
+//Imágenes
 router.post("/publicaciones/imagenes", imagenesController.agregarImagenes) // Agregar imagenes a Publicacion
 router.put("/publicaciones/imagenes/:id", imagenesController.actualizarImagenes) // Actualizar imagenes de Publicacion
 router.put("/publicaciones/borrar_imagenes/:id", imagenesController.borrarImagenes) // Actualizar imagenes con NULL
+
+//Pedidos
 router.post("/pedidos", pedidosController.agregarPedido) // Agregar producto a tabla de pedidos
+
+//Favoritos
 router.post('/favoritos', agregarFavorito);
 router.get('/favoritos/:usuario_id', obtenerFavoritos);
 router.delete('/favoritos/:usuario_id', eliminarFavorito);
+
+
 //RUTAS INEXISTENTES
 router.get("*", (req, res) => {
     res.status(404).send("Esta ruta no existe");
