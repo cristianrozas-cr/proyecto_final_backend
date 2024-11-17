92cc55cf9279d4ec5cda853077f34d2f33bd31f6
Ramiro Sepulveda <rgsepulveda@uc.cl>
2024-11-17 20:34:21 -0300

    jest funcionando con ESM

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,58 +1,62 @@
-import supertest from "supertest";
-import request from 'supertest'; // Importa 'supertest' usando ES Modules
-import server from '../index';   // Importa el servidor
+import request from 'supertest'; // Importa supertest
+import express from 'express';   // Importa la aplicación Express
+import router from '../rutas/rutas';
 
-describe("Operaciones CRUD de cafes", () => {
-  it("Ruta GET devuelve un status 200", async () => {
-    const response = await request(server).get("/cafes").send();
-    const status = response.statusCode;
-    expect(status).toBe(200);
+const app = express();
+
+app.use("/tecno", router);
+
+describe('Test API Endpoints', () => {
+  it('should return a 200 status and a message', async () => {
+    const response = await request(app).get('/tecno/galeria');
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Hello, World!');
   });
 
-  it("Respuesta de ruta GET es un objeto", async () => {
-    const { body } = await request(server).get("/cafes").send();
-    const cafes = body;
-    expect(cafes).toBeInstanceOf(Object);
-  });
+  // it("Respuesta de ruta GET es un objeto", async () => {
+  //   const { body } = await request(app).get("/cafes").send();
+  //   const cafes = body;
+  //   expect(cafes).toBeInstanceOf(Object);
+  // });
 
-  it("Error 404 al intentar borrar un ID inexistente", async () => {
-    const idDeCafeAEliminar = Math.floor(Math.random() * 999);
-    const jwt = "token";
+  // it("Error 404 al intentar borrar un ID inexistente", async () => {
+  //   const idDeCafeAEliminar = Math.floor(Math.random() * 999);
+  //   const jwt = "token";
 
-    const response = await request(server)
-      .delete(`/cafes/${idDeCafeAEliminar}`)
-      .set("Authorization", jwt)
-      .send();
-    const status = response.statusCode;
-    expect(status).toBe(404);
-  });
+  //   const response = await request(app)
+  //     .delete(`/cafes/${idDeCafeAEliminar}`)
+  //     .set("Authorization", jwt)
+  //     .send();
+  //   const status = response.statusCode;
+  //   expect(status).toBe(404);
+  // });
 
-  it("Ruta POST agrega un nuevo cafe", async () => {
-    const id = Math.floor(Math.random() * 999);
-    const nuevoCafe = { id, nombre: "Nuevo Cafe" };
-    const { body: cafes } = await request(server)
-      .post("/cafes")
-      .send(nuevoCafe);
+  // it("Ruta POST agrega un nuevo cafe", async () => {
+  //   const id = Math.floor(Math.random() * 999);
+  //   const nuevoCafe = { id, nombre: "Nuevo Cafe" };
+  //   const { body: cafes } = await request(app)
+  //     .post("/cafes")
+  //     .send(nuevoCafe);
 
-    expect(cafes).toContainEqual(nuevoCafe);
-  });
+  //   expect(cafes).toContainEqual(nuevoCafe);
+  // });
 
-  it("Ruta POST devuelve status 201", async () => {
-    const id = Math.floor(Math.random() * 999);
-    const nuevoCafe = { id, nombre: "Nuevo Cafe" };
-    const response = await request(server).post("/cafes").send(nuevoCafe);
-    const status = response.statusCode;
-    expect(status).toBe(201);
-  });
+  // it("Ruta POST devuelve status 201", async () => {
+  //   const id = Math.floor(Math.random() * 999);
+  //   const nuevoCafe = { id, nombre: "Nuevo Cafe" };
+  //   const response = await request(app).post("/cafes").send(nuevoCafe);
+  //   const status = response.statusCode;
+  //   expect(status).toBe(201);
+  // });
 
-  it("Ruta PUT devuelve status 400 si ID en params es distinto a ID payload", async () => {
-    const idCafeParams = Math.floor(Math.random() * 999);
-    const idPayload = Math.floor(Math.random() * 999);
-    const cafeModificado = { idPayload, nombre: "Cafe Modificado" };
-    const response = await request(server)
-      .put(`/cafes/${idCafeParams}`)
-      .send(cafeModificado);
-    const status = response.statusCode;
-    expect(status).toBe(400);
-  });
+  // it("Ruta PUT devuelve status 400 si ID en params es distinto a ID payload", async () => {
+  //   const idCafeParams = Math.floor(Math.random() * 999);
+  //   const idPayload = Math.floor(Math.random() * 999);
+  //   const cafeModificado = { idPayload, nombre: "Cafe Modificado" };
+  //   const response = await request(app)
+  //     .put(`/cafes/${idCafeParams}`)
+  //     .send(cafeModificado);
+  //   const status = response.statusCode;
+  //   expect(status).toBe(400);
+  // });
 });
